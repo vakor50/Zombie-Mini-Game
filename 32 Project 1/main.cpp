c3103b80636fe4bf73540755a567407fd3d95dd0
Nitish Thakor <Nitish@dhcp125-054.seas.ucla.edu>
2014-01-08 11:58:42 -0800

    Initial Commit

---
+++ 32 Project 1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  32 Project 1
+//
+//  Created by Nitish Thakor on 1/8/14.
+//  Copyright (c) 2014 Vir Thakor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
